c92a963398628129257449f59a34d1e19e7b6e86
Viviana Velez Montoya <vivianavelezm@hotmail.com>
2022-10-18 21:42:08 +0200

    done

--- vue-project-casApp/src/App.vue
+++ vue-project-casApp/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import navBar from './components/navBar.vue';
+import logIn from './components/logIn.vue';
 </script>
 
 <template>
 
 <navBar/>
+<logIn/>
  
 </template>
 
--- vue-project-casApp/src/components/navBar.vue
+++ vue-project-casApp/src/components/navBar.vue
@@ -1,7 +1,43 @@
 <template>
 
-<b-navbar toggleable="md" type="danger" variant="danger">
+<b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand class="d-inline-block align-top" href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-triangle-half" viewBox="0 0 16 16">
+        <path d="M8.065 2.016A.13.13 0 0 0 8.002 2v11.983l6.856.017a.12.12 0 0 0 .066-.017.162.162 0 0 0 .054-.06.176.176 0 0 0-.002-.183L8.12 2.073a.146.146 0 0 0-.054-.057zm-1.043-.45a1.13 1.13 0 0 1 1.96 0l6.856 11.667c.458.778-.091 1.767-.98 1.767H1.146c-.889 0-1.437-.99-.98-1.767L7.022 1.566z"/>
+        </svg>
+        casApp
+    </b-navbar-brand>
 
-</b-navbar>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="{name:'home'}" exact>Home</b-nav-item>
+        <b-nav-item :to="{name:'about'}">About</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown right>
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+
 
 </template>
+
+<style>
+svg{
+    height: 30px;
+    width: 30px;
+    align-items: center;
+    color: aqua;
+}
+
+</style>

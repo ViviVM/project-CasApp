0c0311758e4bc8e89c904e8784882a732075b4d8
Viviana Velez Montoya <vivianavelezm@hotmail.com>
2022-10-22 16:39:48 +0200

    done

--- vue-project-casApp/src/components/task.vue
+++ vue-project-casApp/src/components/task.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="container-sm">
         <h1>Task</h1>
-        <div class="position-relative my-4 pb-4 pt-4 px-1">
-            <button type="button" class="btn btn-success btn-sm position-absolute top-50 start-100 translate-middle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-            </svg>
-        <a class="pb-2 pt-2 px-2" href="/newtask">New task</a>
-        </button>
+        <div class="position-relative pb-4 pt-4 ">
+            <button type="button" class="btn btn-success position-absolute top-50 start-100 translate-middle">
+                <a href="/newtask">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                    New task</a>
+            </button>
         </div>
         
         <table class="table table-striped table-hover">
@@ -77,6 +78,15 @@
 </template>
 
 <style>
+
+.btn-app-primary{
+    --bs-btn-bg:  var(--bd-violet);
+    --bs-btn-border-color:#65b8a6;
+    --bs-btn-color: aliceblue;
+    --bs-btn-hover-color: aliceblue;
+
+    
+}
 
 .btn a {
     color: aliceblue;
